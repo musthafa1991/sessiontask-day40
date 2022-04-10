b18604a0d1c1e9d5c833dbc2462ba29697a8d954
Mohamed Musthafa <musthafam39@gmail.com>
2022-04-10 12:22:42 +0530

    formatted

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 
-const fs=require("fs");
+const fs = require("fs");
 
-const express=require("express");
-const app=express();
+const express = require("express");
+const app = express();
 
 
 
-app.get("/createfile",function(req,res){
-    
-    const d=new Date();
-    let filename =`${d.getDate()}-${(d.getMonth() + 1)}-${d.getFullYear()}-${d.getHours()}.${d.getMinutes()}`;
+app.get("/createfile", function (req, res) {
+
+    const d = new Date();
+    let filename = `${d.getDate()}-${(d.getMonth() + 1)}-${d.getFullYear()}-${d.getHours()}.${d.getMinutes()}`;
     // filename=filename.toString();
     console.log(filename)
 
-    let timestamp=Math.floor(new Date().getTime() / 1000).toString();
+    let timestamp = Math.floor(new Date().getTime() / 1000).toString();
 
 
-    fs.writeFile(`./${filename}.txt`,timestamp, function(err) {
-        if(err) {
+    fs.writeFile(`./${filename}.txt`, timestamp, function (err) {
+        if (err) {
             return console.log(err);
         }
         console.log("The file was saved!");
-    }); 
+    });
 
-   res.send("file created");
+    res.send("file created");
 })
 app.listen(4000)
